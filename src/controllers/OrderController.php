ac9cf36ee5aa50220d86a4c7c029c324c9467971
Andrii Vasyliev <sol@hiqdev.com>
2017-05-19 09:00:01 +0000

    tested configured commands

--- src/controllers/OrderController.php
+++ src/controllers/OrderController.php
@@ -4,4 +4,13 @@ namespace hiqdev\billing\hiapi\controllers;
 
 class OrderController extends \hiapi\controllers\BaseController
 {
+    public function commands()
+    {
+        return array_merge(parent::commands(), [
+            'ping' => [
+                'class'  => \hiapi\commands\PingCommand::class,
+                'answer' => 'order BANG',
+            ],
+        ]);
+    }
 }
