05f0c66890c6d446960b7b3a8a6bd5393eaab73d
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2017-06-06 16:53:37 +0300

    Updated to follow NestedModelValidator API changes

--- src/commands/order/ActionDto.php
+++ src/commands/order/ActionDto.php
@@ -13,30 +13,12 @@ class ActionDto extends Model
 
     public $quantity;
 
-    public function load($data, $formName = '')
-    {
-        if (isset($data['quantity'])) {
-            $quantity = new QuantityDto();
-            $quantity->load($data['quantity']);
-
-            $data['quantity'] = $quantity;
-        }
-
-        if (isset($data['target'])) {
-            $target = new TargetDto();
-            $target->load($data['target']);
-
-            $data['target'] = $target;
-        }
-
-        return $this->setAttributes($data);
-    }
-
     public function rules()
     {
         return [
             [['type', 'target', 'quantity'], 'required'],
-            [['target', 'quantity'], NestedModelValidator::class],
+            [['target'], NestedModelValidator::class, 'modelClass' => TargetDto::class],
+            [['quantity'], NestedModelValidator::class, 'modelClass' => QuantityDto::class],
             [['type'], 'string']
         ];
     }
--- src/commands/order/CalculateValueCommand.php
+++ src/commands/order/CalculateValueCommand.php
@@ -9,25 +9,11 @@ class CalculateValueCommand extends BaseCommand
 {
     public $items;
 
-    public function load($data, $formName = '')
-    {
-        if (isset($data['items'])) {
-            $items = [];
-            foreach ($data['items'] as $item) {
-                $items[] = $action = new ActionDto();
-                $action->load($item);
-            }
-            $data['items'] = $items;
-        }
-
-        return $this->setAttributes($data);
-    }
-
     public function rules()
     {
         return [
             ['items', 'required'],
-            ['items', 'each', 'rule' => [NestedModelValidator::class]],
+            ['items', 'each', 'rule' => [NestedModelValidator::class, 'modelClass' => ActionDto::class]],
         ];
     }
 }
