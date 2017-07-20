e918d36c7c0a737e618032742fcb6f12e5bcdb22
Andrii Vasyliev <sol@hiqdev.com>
2017-07-20 15:28:50 +0000

    fixed building sql with passing parameters instead of possible injection

--- src/plan/AvailableForField.php
+++ src/plan/AvailableForField.php
@@ -29,21 +29,24 @@ class AvailableForField implements FieldInterface
 
     public function buildCondition($value)
     {
+        $params = [];
         if ($value['client_id']) {
+            $params[':available_for_client_id'] = $value['client_id'];
             $ids_sql = "
                 SELECT      tariff_id
                 FROM        client2tariff
-                WHERE       client_id={$value['client_id']}
+                WHERE       client_id=:available_for_client_id
             ";
         } else {
+            $params[':available_for_seller'] = $value['seller'];
             $ids_sql = "
                 SELECT      dst_id
                 FROM        tie
-                WHERE       src_id=client_id('{$value['seller']}')
+                WHERE       src_id=client_id(:available_for_seller)
                         AND tag_id=type_id('tariff')
             ";
         }
 
-        return new Expression("zt.obj_id IN ($ids_sql)");
+        return new Expression("zt.obj_id IN ($ids_sql)", $params);
     }
 }
