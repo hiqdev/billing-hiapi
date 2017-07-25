dbaf03ac140dea8c0f8f8d73892d4bbd25ce776b
Andrii Vasyliev <sol@hiqdev.com>
2017-07-25 18:48:34 +0000

    fixed

--- src/query/BillQuery.php
+++ src/query/BillQuery.php
@@ -9,21 +9,8 @@ class BillQuery extends \hiapi\query\Query
 {
     protected $modelClass = Bill::class;
 
-    public function initFrom()
-    {
-        return $this->from('zbill   zb')
-            ->leftJoin('zref        bt', 'bt.obj_id = zb.type_id')
-            ->leftJoin('purse       zp', 'zp.obj_id = zb.purse_id')
-            ->leftJoin('zclient     zc', 'zc.obj_id = zp.client_id')
-            ->leftJoin('zclient     cr', 'cr.obj_id = zc.seller_id')
-            ->leftJoin('zref        cu', 'cu.obj_id = zp.currency_id')
-            ->leftJoin('obj         zo', 'zo.obj_id = zb.object_id')
-            ->leftJoin('zref        oc', 'oc.obj_id = zo.class_id')
-        ;
-    }
-
     /**
-     * @return mixed
+     * @return array
      */
     protected function attributesMap()
     {
@@ -53,5 +40,18 @@ class BillQuery extends \hiapi\query\Query
                 'type' => 'oc.name',
             ],
         ];
+    }
+
+    public function initFrom()
+    {
+        return $this->from('zbill   zb')
+            ->leftJoin('zref        bt', 'bt.obj_id = zb.type_id')
+            ->leftJoin('purse       zp', 'zp.obj_id = zb.purse_id')
+            ->leftJoin('zclient     zc', 'zc.obj_id = zp.client_id')
+            ->leftJoin('zclient     cr', 'cr.obj_id = zc.seller_id')
+            ->leftJoin('zref        cu', 'cu.obj_id = zp.currency_id')
+            ->leftJoin('obj         zo', 'zo.obj_id = zb.object_id')
+            ->leftJoin('zref        oc', 'oc.obj_id = zo.class_id')
+        ;
     }
 }
