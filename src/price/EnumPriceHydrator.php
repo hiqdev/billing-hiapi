820b0ecb15c61d32d38991b3988347cb170a9aa8
Andrii Vasyliev <sol@hiqdev.com>
2018-06-04 14:20:26 +0000

    added `EnumPriceHydrator`

---
+++ src/price/EnumPriceHydrator.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * API for Billing
+ *
+ * @link      https://github.com/hiqdev/billing-hiapi
+ * @package   billing-hiapi
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017-2018, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\billing\hiapi\price;
+
+/**
+ * EnumPrice Hydrator.
+ *
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+class EnumPriceHydrator extends PriceHydrator
+{
+    /**
+     * {@inheritdoc}
+     * @param object|Plan $object
+     */
+    public function extract($object)
+    {
+        return array_merge(parent::extract($object), array_filter([
+            'unit'          => $this->hydrator->extract($object->getUnit()),
+            'currency'      => $this->hydrator->extract($object->getCurrency()),
+            'sums'          => $sums,
+        ]));
+    }
+}
