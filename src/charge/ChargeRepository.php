72e76d77806919f852273efb11c6b8ba8d39aeb7
Andrey Klochok <andreyklochok@gmail.com>
2018-07-03 19:40:59 +0300

    Save Action when save charge if Action has sale

--- src/charge/ChargeRepository.php
+++ src/charge/ChargeRepository.php
@@ -38,18 +38,16 @@ class ChargeRepository extends BaseRepository
     public function save(Charge $charge)
     {
         $action = $charge->getAction();
+        $tariff_id = null;
         if ($action->hasSale($action)) {
-            $sale = $action->getSale();
-        } else {
-            $sale = new Sale(null, $action->getTarget(), $action->getCustomer(), $charge->getPrice()->getPlan());
-            $action->setSale($sale);
+            $tariff_id = $action->getSale()->getPlan()->getId();
+            $this->em->save($action);
         }
-        $this->em->save($action);
         $target = $this->generalizer->lessGeneral($charge->getAction()->getTarget(), $charge->getPrice()->getTarget());
         $hstore = new HstoreExpression(array_filter([
             'id'            => $charge->getId(),
             'object_id'     => $target->getId(),
-            'tariff_id'     => $sale->getPlan()->getId(),
+            'tariff_id'     => $tariff_id,
             'action_id'     => $action->getId(),
             'type_id'       => $action->getType()->getId(),
             'type'          => $action->getType()->getName(),
