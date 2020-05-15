4c6b87a43772f660ebb925f7261002c8eb0d851c
Andrii Vasyliev <sol@hiqdev.com>
2020-05-15 13:12:02 +0000

    Add `TargetRepository::save()`

--- src/target/TargetRepository.php
+++ src/target/TargetRepository.php
@@ -11,6 +11,8 @@
 namespace hiqdev\billing\hiapi\target;
 
 use hiqdev\php\billing\target\Target;
+use hiqdev\php\billing\target\TargetInterface;
+use hiqdev\php\billing\target\TargetRepositoryInterface;
 use hiqdev\yii\DataMapper\query\Specification;
 
 /**
@@ -18,12 +20,28 @@ use hiqdev\yii\DataMapper\query\Specification;
  *
  * @author Dmytro Naumenko <d.naumenko.a@gmail.com>
  */
-class TargetRepository extends \hiqdev\yii\DataMapper\repositories\BaseRepository
+class TargetRepository extends \hiqdev\yii\DataMapper\repositories\BaseRepository implements TargetRepositoryInterface
 {
     public $queryClass = TargetQuery::class;
 
     public function findOneById($id): ?Target
     {
         return $this->findOne((new Specification())->andWhere(['id' => $id]));
+    }
+
+    public function save(TargetInterface $target)
+    {
+        $params = [
+            ':type'         => $target->getType(),
+            ':name'         => $target->getName(),
+            ':remoteid'     => $target->getRemoteId(),
+            ':client_id'    => $target->getCustomer()->getId(),
+        ];
+        $command = $this->db->createCommand('
+            INSERT INTO target (client_id, type_id, name, remoteid)
+            VALUES (:client_id, class_id(:type), :name, :remoteid)
+            RETURNING obj_id
+        ', $params);
+        $target->setId($command->queryScalar());
     }
 }
