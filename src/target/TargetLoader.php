f9f214e772bb5d87416686963634f708e3d90b99
Andrii Vasyliev <sol@hiqdev.com>
2020-06-08 10:34:38 +0000

    Improving `TargetLoader` to find target by name too

--- src/target/TargetLoader.php
+++ src/target/TargetLoader.php
@@ -13,6 +13,7 @@ namespace hiqdev\billing\hiapi\target;
 use hiqdev\php\billing\target\TargetInterface;
 use hiqdev\php\billing\target\TargetRepositoryInterface;
 use League\Tactician\Middleware;
+use hiqdev\DataMapper\Query\Specification;
 
 class TargetLoader implements Middleware
 {
@@ -34,10 +35,17 @@ class TargetLoader implements Middleware
 
     private function findTarget($command): ?TargetInterface
     {
-        if (empty($command->target_id)) {
+        if (!empty($command->target_id)) {
+            $cond = ['id' => $command->target_id];
+        } elseif (!empty($command->target_type) && !empty($command->target_name)) {
+            $cond = [
+                'type' => $command->target_type,
+                'name' => $command->target_name,
+            ];
+        } else {
             return null;
         }
 
-        return $this->repo->findById($command->target_id);
+        return $this->repo->findOne((new Specification)->where($cond));
     }
 }
