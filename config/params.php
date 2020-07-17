c2d0089a8f7239e9ee189664f3bb7e17a29add2b
Andrii Vasyliev <sol@hiqdev.com>
2020-07-17 09:32:49 +0000

    Move price factory configuration to `billing-mrdp`

--- config/params.php
+++ config/params.php
@@ -8,17 +8,9 @@
  * @copyright Copyright (c) 2017-2020, HiQDev (http://hiqdev.com/)
  */
 
-use hiqdev\billing\hiapi\price\TemplatePrice;
-use hiqdev\php\billing\price\EnumPrice;
 use hiqdev\php\billing\price\SinglePrice;
 
 return [
-    'billing-hiapi.price.types' => [
-        'certificate_purchase'              => EnumPrice::class,
-        'certificate_renewal'               => EnumPrice::class,
-        'certificate,certificate_purchase'  => EnumPrice::class,
-        'certificate,certificate_renewal'   => EnumPrice::class,
-        \hiqdev\billing\hiapi\price\TemplatePriceDto::class => TemplatePrice::class,
-    ],
+    'billing-hiapi.price.types' => [],
     'billing-hiapi.price.defaultClass' => SinglePrice::class,
 ];
