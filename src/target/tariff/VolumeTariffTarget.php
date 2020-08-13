e162bce6ed5a19fcdf12df5e61688a96320e92d7
Andrey Klochok <andreyklochok@gmail.com>
2020-08-13 17:49:15 +0300

    Added targets for new plans

---
+++ src/target/tariff/VolumeTariffTarget.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * API for Billing
+ *
+ * @link      https://github.com/hiqdev/billing-hiapi
+ * @package   billing-hiapi
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017-2020, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\billing\hiapi\target\tariff;
+
+class VolumeTariffTarget extends TariffTarget
+{
+}
