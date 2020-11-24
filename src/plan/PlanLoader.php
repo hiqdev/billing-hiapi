77d9ff480b0fd788056e86c32c3fba2f7c8d1574
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2020-11-24 19:09:15 +0200

    Enhance error handling

--- src/plan/PlanLoader.php
+++ src/plan/PlanLoader.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 /**
  * API for Billing
  *
@@ -11,6 +12,7 @@
 namespace hiqdev\billing\hiapi\plan;
 
 use hiapi\exceptions\domain\RequiredInputException;
+use hiapi\exceptions\domain\ValidationException;
 use hiqdev\DataMapper\Query\Specification;
 use hiqdev\php\billing\plan\PlanInterface;
 use hiqdev\php\billing\plan\PlanRepositoryInterface;
@@ -18,9 +20,8 @@ use League\Tactician\Middleware;
 
 class PlanLoader implements Middleware
 {
-    private $repo;
-
-    public $isRequired = false;
+    private PlanRepositoryInterface $repo;
+    public bool $isRequired = false;
 
     public function __construct(PlanRepositoryInterface $repo)
     {
@@ -32,7 +33,18 @@ class PlanLoader implements Middleware
         if (empty($command->plan)) {
             $command->plan = $this->findPlan($command);
             if ($this->isRequired && empty($command->plan)) {
-                throw new RequiredInputException('plan');
+                if (!empty($command->plan_id)) {
+                    throw new ValidationException(
+                        sprintf('Failed to find plan by ID %s: wrong ID or not authorized', $command->plan_id)
+                    );
+                }
+                if (!empty($command->plan_name)) {
+                    throw new ValidationException(
+                        sprintf('Failed to find plan by name "%s": wrong name or not authorized', $command->plan_name)
+                    );
+                }
+
+                throw new RequiredInputException('plan_id or plan_name');
             }
         }
 
@@ -51,11 +63,11 @@ class PlanLoader implements Middleware
             return null;
         }
 
-        return $this->repo->findOne((new Specification)->where($cond));
+        return $this->repo->findOne((new Specification)->where($cond)) ?: null;
     }
 
     private function getSeller($command): ?string
     {
-        return $command->customer->getSeller()->getLogin() ?? null;
+        return $command->customer->getSeller()->getLogin();
     }
 }
