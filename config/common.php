49c96426bbec7d37950dcc7bbc62d7ce7ae8bf15
Andrey Klochok <andreyklochok@gmail.com>
2020-11-27 18:58:35 +0200

    Add save requisites for Bill

--- config/common.php
+++ config/common.php
@@ -25,6 +25,7 @@ $singletons = [
             \hiqdev\php\billing\charge\ChargeState::class       => \hiqdev\billing\hiapi\charge\ChargeStateHydrator::class,
             \hiqdev\php\billing\bill\Bill::class                => \hiqdev\billing\hiapi\bill\BillHydrator::class,
             \hiqdev\php\billing\bill\BillState::class           => \hiqdev\billing\hiapi\bill\BillStateHydrator::class,
+            \hiqdev\php\billing\bill\BillRequisite::class       => \hiqdev\billing\hiapi\bill\BillRequisiteHydrator::class,
             \hiqdev\php\billing\target\Target::class            => \hiqdev\billing\hiapi\target\TargetHydrator::class,
             \hiqdev\php\billing\target\TargetCollection::class  => \hiqdev\billing\hiapi\target\TargetHydrator::class,
             \hiqdev\php\billing\type\Type::class                => \hiqdev\billing\hiapi\type\TypeHydrator::class,
---
+++ src/bill/BillRequisiteHydrator.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * API for Billing
+ *
+ * @link      https://github.com/hiqdev/billing-hiapi
+ * @package   billing-hiapi
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017-2020, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\billing\hiapi\bill;
+
+use hiqdev\php\billing\bill\BillRequisite;
+use hiqdev\DataMapper\Hydrator\GeneratedHydrator;
+
+class BillRequisiteHydrator extends GeneratedHydrator
+{
+    public function hydrate(array $data, $object)
+    {
+        return parent::hydrate($data, $object);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @param BillRequisite $object
+     */
+    public function extract($object)
+    {
+        return array_filter([
+            'id' => $object->getId(),
+            'name' => $object->getName(),
+        ]);
+    }
+}
