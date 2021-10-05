ed0ac026b75fdc2a203c94cf90c42b84a43fee0d
strorch <smy980807@ukr.net>
2021-10-05 20:22:29 +0300

    added customers remote account sync to plans search command

--- src/plan/Search/BulkBuilder.php
+++ src/plan/Search/BulkBuilder.php
@@ -14,6 +14,7 @@ use hiapi\Core\Endpoint\BuilderFactory;
 use hiapi\Core\Endpoint\Endpoint;
 use hiapi\Core\Endpoint\EndpointBuilder;
 use hiapi\endpoints\Module\Multitenant\Tenant;
+use hiqdev\billing\hiapi\customer\CustomerLoader;
 use hiqdev\php\billing\plan\Plan;
 
 final class BulkBuilder
@@ -31,6 +32,7 @@ final class BulkBuilder
                      ->take(Command::class)
                      ->checkPermission('plan.read')
                      ->middlewares(
+                         CustomerLoader::class,
                          $build->call(BulkAction::class)
                      )
                      ->return($build->many(Plan::class));
