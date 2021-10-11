e597e199784db7027ee17efaddbc01aa55f7ab6c
strorch <smy980807@ukr.net>
2021-10-11 18:58:48 +0300

    minor

--- src/plan/Search/BulkAction.php
+++ src/plan/Search/BulkAction.php
@@ -11,6 +11,7 @@
 namespace hiqdev\billing\hiapi\plan\Search;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use hiapi\Core\Auth\AuthRule;
 use hiqdev\billing\hiapi\plan\AvailableFor;
 use hiqdev\billing\hiapi\plan\PlanReadModelRepositoryInterface;
 use yii\web\User;
@@ -18,15 +19,24 @@ use yii\web\User;
 class BulkAction
 {
     private PlanReadModelRepositoryInterface $repo;
+    private User $user;
 
-    public function __construct(PlanReadModelRepositoryInterface $repo)
+    public function __construct(PlanReadModelRepositoryInterface $repo, User $user)
     {
         $this->repo = $repo;
+        $this->user = $user;
     }
 
     public function __invoke(Command $command): ArrayCollection
     {
         $spec = $command->getSpecification();
+
+        if (
+            empty($spec->where[AvailableFor::CLIENT_ID_FIELD])
+            && empty($spec->where[AvailableFor::SELLER_FIELD])
+        ) {
+            $spec->where['client_id'] = AuthRule::clientId($this->user->id)->canSeeSellerObjects();
+        }
 
         $res = $this->repo->findAll($spec);
 
