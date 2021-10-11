071bdf336fe3115448a537dee006a657695a0c19
strorch <smy980807@ukr.net>
2021-10-11 17:41:50 +0300

    fixed logic conflict with plansSearch and api/v1/plans

--- src/plan/Search/BulkAction.php
+++ src/plan/Search/BulkAction.php
@@ -18,21 +18,15 @@ use yii\web\User;
 class BulkAction
 {
     private PlanReadModelRepositoryInterface $repo;
-    private User $user;
 
-    public function __construct(PlanReadModelRepositoryInterface $repo, User $user)
+    public function __construct(PlanReadModelRepositoryInterface $repo)
     {
         $this->repo = $repo;
-        $this->user = $user;
     }
 
     public function __invoke(Command $command): ArrayCollection
     {
         $spec = $command->getSpecification();
-
-        if (empty($command->where[AvailableFor::SELLER_FIELD])) {
-            $spec->where[AvailableFor::CLIENT_ID_FIELD] ??= $this->user->id;
-        }
 
         $res = $this->repo->findAll($spec);
 
